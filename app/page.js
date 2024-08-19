e71a2297e59d02b6e53cd218585768f7f2de357f
Temu-Lala <temesgendebebe1921@gmail.com>
2024-08-19 04:54:05 +0300

    first commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import Htmls from './pages/htmls/page.js'
+import Htmls from '../app/pages/htmls'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-
+<Htmls/>
     </main>
   );
 }
